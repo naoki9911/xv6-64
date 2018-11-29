6b976b7304e78b73017b2252db8b159d8a06c2a4
Naoki Matsumoto <m.naoki9911@gmail.com>
2018-11-29 22:57:29 +0900

    [update] update build.sh to build kernel

--- README.md
+++
@@ -1,2 +0,0 @@
-# xv6-64
-xv6-64 is the port of xv6 for x86-64 architecture and UEFI
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ ! -e image/EFI/BOOT ]; then
+  mkdir -p image/EFI/BOOT
+fi
+
 cd edk2
 source edksetup.sh
 build -p bootloader/xv6-64_loader.dsc
+
+cd ../os
+make
+cp kernel ../image/.
