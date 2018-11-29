19b1a17219f6e002e9e754078813bdc37e04bb59
Naoki Matsumoto <m.naoki9911@gmail.com>
2018-11-29 23:04:11 +0900

    [update] entry UEFI bootloader into main.c

---
+++ bootloader/main.c
@@ -0,0 +1,24 @@
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Protocol/BlockIo.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Library/DevicePathLib.h>
+#include  <Guid/FileInfo.h>
+#include  <Guid/Acpi.h>
+#include  <Library/MemoryAllocationLib.h>
+#include  <Library/BaseMemoryLib.h>
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE ImageHandle,
+  IN EFI_SYSTEM_TABLE *SystemTable
+  )
+{
+  Print(L"Hello UEFI!\n");
+  while(1){
+    asm("hlt");
+  }
+}
--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ if [ ! -e image/EFI/BOOT ]; then
   mkdir -p image/EFI/BOOT
 fi
 
+if [ -e bootloader/build ]; then
+#        rm -rf bootloader/build/*
+fi
+
 cd edk2
 source edksetup.sh
 build -p bootloader/xv6-64_loader.dsc
